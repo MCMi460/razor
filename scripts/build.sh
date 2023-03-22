6980d501b3008f8a34437bd2f74acb5ea9f94567
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-03-22 17:12:35 -0500

    Change name

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,5 +12,5 @@ fi
 
 # Actual building
 python3 -m pip install -r requirements.txt py2app # Install requirements + py2app
-python3 setup.py py2app -O2 # Build
+python3 setupMac.py py2app -O2 # Build
 open dist # Open for user's benefit
