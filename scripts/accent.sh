934f4c78bdd6a4f026d85a18edee0a540df940c0
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-07-27 22:42:17 -0500

    Add accent color files
    Co-Authored-By: Spotlight <spotlight@joscomputing.space>

---
+++ scripts/accent.sh
@@ -0,0 +1,3 @@
+# Compile Asset Catalog containing red accent color via actool
+# This creates Assets.car.
+actool --platform macosx --minimum-deployment-target 10.15 --compile .. ../AccentColor.xcassets
